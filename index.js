e81425d7b064da9399853ebcdc66c5fd81fe18e8
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro.local>
2015-09-07 16:59:54 -0700

    added code to create session and for get routes

--- index.js
+++ index.js
@@ -1,27 +1,113 @@
-//REQUIREMENTS
+//========================================
+// <<<<<<<<<<< requirements >>>>>>>>>>>>>>
+//========================================
 var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'), 
 	_ = require("underscore"),
 	path = require("path"),
+	// views path
 	views = path.join(process.cwd(), "views/"),
 	//require db models
-	db = require("./models");
+	db = require("./models"),
+	//requires express-session
+	session = require("express-session");
 
-//CONFIG
+//========================================
+// <<<<<<<<<<<<<< config >>>>>>>>>>>>>>>>>
+//========================================
 //serve js & css files
 app.use("/static", express.static("public"));
 app.use("/vendor", express.static("bower_components"));
-//body parser config to accept all datatypes
-app.use(bodyParser.urlencoded({extended: true}))
 
-//ROUTES
+//body parser config to accept all datatypes
+app.use(bodyParser.urlencoded({extended: true}));
+
+//=======================================
+// <<<<<<<<<< create session >>>>>>>>>>>>
+//========================================
+
+app.use(
+	session({
+		secret: 'super-secret-private-keyyy',
+		resave: false,
+		saveUninitialized: true
+	})
+
+);
+
+// extending the `req` object to help manage sessions
+app.use(function (req, res, next) {
+  // login a user
+  req.login = function (user) {
+    req.session.userId = user._id;
+  };
+  // find the current user
+  req.currentUser = function (cb) {
+    db.User.
+      findOne({ _id: req.session.userId },
+      function (err, user) {
+        req.user = user;
+        cb(null, user);
+      })
+  };
+  // logout the current user
+  req.logout = function () {
+    req.session.userId = null;
+    req.user = null;
+  }
+  // call the next middleware in the stack
+  next(); 
+});
+
+// show the current user
+app.get("/profile", function userShow(req, res) {
+  req.currentUser(function (err, user) {
+    res.send("Hello " + user.email);
+  })
+});
+
+//========================================
+// <<<<<<<<<<<< get routes >>>>>>>>>>>>>>>
+//========================================
+
+//home route
 app.get("/", function (req, res) {
 	res.send("Hello world!");
 });
 
+//login route
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(views, "login.html"));
+});
+
+//signup route
+app.get("/signup", function (req, res) {
+  res.sendFile(path.join(views, "signup.html"));
+});
+
+//profile route
+app.get("/profile", function (req, res) {
+  res.sendFile(path.join(views, "profile.html"));
+});
+
+//bar route
+app.get("/profile", function (req, res) {
+  res.sendFile(path.join(views, "bar.html"));
+});
+
+//recipe route
+app.get("/profile", function (req, res) {
+  res.sendFile(path.join(views, "recipe.html"));
+});
+
+//========================================
+// <<<<<<<<<<<< API endpoints >>>>>>>>>>>>
+//========================================
+
 //where the user submits the sign-up form
 app.post(["/users", "/signup"], function signup(req, res) {
+	console.log("Looks like you're trying to signup!");
 	//grab the user from the params
 	var user = req.body.user;
 	//pull out their email & password
@@ -33,14 +119,21 @@ app.post(["/users", "/signup"], function signup(req, res) {
 	});
 });
 
+// where the user submits the login form
 app.post(["/sessions", "/login"], function login(req, res) {
   var user = req.body.user;
   var email = user.email;
   var password = user.password;
   db.User.authenticate(email, password, function (err, user) {
-    res.send(email + " is logged in\n");
+    // login the user
+    req.login(user);
+    console.log("User is logged in");
+    // redirect to user profile
+    res.redirect("/profile"); 
   });
 });
+
+
 
 
 //SPECIFYING PORT
