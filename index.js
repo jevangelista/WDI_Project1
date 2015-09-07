a99861f7b600e0b57c841cdb2c6f52c5bc6ecc40
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro.local>
2015-09-07 00:20:40 -0700

    added model methods, signup route, and login route

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var express = require('express'),
 	bodyParser = require('body-parser'), 
 	_ = require("underscore"),
 	path = require("path"),
-	views = path.join(process.cwd(), "views/");
+	views = path.join(process.cwd(), "views/"),
+	//require db models
+	db = require("./models");
 
 //CONFIG
 //serve js & css files
@@ -17,6 +19,29 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.get("/", function (req, res) {
 	res.send("Hello world!");
 });
+
+//where the user submits the sign-up form
+app.post(["/users", "/signup"], function signup(req, res) {
+	//grab the user from the params
+	var user = req.body.user;
+	//pull out their email & password
+	var email = user.email;
+	var password = user.password;
+	//create the new user
+	db.User.createSecure(email,password, function() {
+		res.send(email + " is registered!\n");
+	});
+});
+
+app.post(["/sessions", "/login"], function login(req, res) {
+  var user = req.body.user;
+  var email = user.email;
+  var password = user.password;
+  db.User.authenticate(email, password, function (err, user) {
+    res.send(email + " is logged in\n");
+  });
+});
+
 
 //SPECIFYING PORT
 var listener = app.listen(3000, function() {
